1fd2c615eb7285b000bdd5de13b490f1d883e5c2
Sergi van Ravenswaay <contact@ikbensergi.com>
2018-01-26 23:22:13 +0100

    New managers and scene updates added

--- GlobalGameJam2018/Assets/2. Scripts/Manager/Event_Manager.cs
+++ GlobalGameJam2018/Assets/2. Scripts/Manager/Event_Manager.cs
@@ -3,14 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Event_Manager : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public delegate void Time_Changed(Time_States timestate);
+    public static event Time_Changed TimeChange;
+    public static void Time_Change(Time_States _timestate) {
+        TimeChange(_timestate);
+        }
 }
