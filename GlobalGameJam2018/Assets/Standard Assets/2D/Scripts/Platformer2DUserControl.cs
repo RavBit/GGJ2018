a7a65449126142307c452ffa27b064dd69a37f85
FransHuntink <boebout@gmail.com>
2018-01-26 22:37:43 +0100

    V1 - Basic input added
    Basic input (movement/jump) added.

--- GlobalGameJam2018/Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
+++ GlobalGameJam2018/Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -21,9 +21,13 @@ namespace UnityStandardAssets._2D
         {
             if (!m_Jump)
             {
-                // Read the jump input in Update so button presses aren't missed.
-                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+         
             }
+        }
+
+        public void performJump()
+        {
+            m_Jump = true;
         }
 
 
@@ -31,7 +35,14 @@ namespace UnityStandardAssets._2D
         {
             // Read the inputs.
             bool crouch = Input.GetKey(KeyCode.LeftControl);
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+
+            //HACKED: Takes acceleration from x axis as horizontal movement instead of inputManager.
+            // float h = CrossPlatformInputManager.GetAxis("Horizontal");
+
+            float h = Input.acceleration.x;
+            if(h < -1) {  h = -1;}
+            if(h >  1) { h = 1; }
+
             // Pass all parameters to the character control script.
             m_Character.Move(h, crouch, m_Jump);
             m_Jump = false;
