b563cd03d041a6ec4ae65d5019625825f78b277f
Sergi van Ravenswaay <contact@ikbensergi.com>
2018-01-27 13:28:57 +0100

    Fix gears and head, working on platforms

---
+++ GlobalGameJam2018/Assets/Moving_Platform.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving_Platform : MonoBehaviour {
+    void OnCollisionStay2D(Collision2D coll) {
+        if (coll.gameObject.tag == "Player") {
+            Debug.Log("Collision");
+            coll.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(10, 0);
+        }
+    }
+}
