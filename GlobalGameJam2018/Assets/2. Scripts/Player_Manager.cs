ed888c2982cd6bbeed9f66137a3855ad125ce36e
FransHuntink <boebout@gmail.com>
2018-01-27 16:13:00 +0100

    Local opt

--- GlobalGameJam2018/Assets/2. Scripts/Player_Manager.cs
+++ GlobalGameJam2018/Assets/2. Scripts/Player_Manager.cs
@@ -6,10 +6,15 @@ public class Player_Manager : MonoBehaviour {
     public GameObject Head;
     public Transform HeadLocation;
     public Transform EndPosition;
+
+    private Vector2 startPos;
     public Rigidbody2D RB;
     private void Start() {
         Event_Manager.GetHead += GetHead;
         Event_Manager.SetHead += SetHead;
+
+        startPos = gameObject.transform.position;
+       
     }
     void Update() {
         RB.mass = Game_Manager.instance.Gravity;
@@ -17,6 +22,12 @@ public class Player_Manager : MonoBehaviour {
         if (speed >= 1)
             speed = -1;
         Head.transform.Rotate(0, 0, 20 * speed * Time.deltaTime);
+
+        //temp respawn to same place only above platform when you fall
+    if(transform.position.y < -18)
+        {
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x, 12);
+        }
     }
 
     public GameObject GetHead() {
