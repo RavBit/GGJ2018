9219da0fc55f2f3ece81413723a6e5b2d03ee8e4
FransHuntink <boebout@gmail.com>
2018-01-27 15:06:15 +0100

    Fuck you

---
+++ GlobalGameJam2018/Assets/2. Scripts/Gears/TurnGearScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnGearScript : MonoBehaviour {
+    public float gearSpeed;
+    public float multiply;
+	// Use this for initialization
+	void Start () {
+        Debug.Log(transform.localScale.x);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, 0, (gearSpeed * transform.localScale.x) * multiply);
+	}
+}
