c50c0cc0f92aa2866ed0e2ceb11ead7acc6eb4cc
FransHuntink <boebout@gmail.com>
2018-01-27 13:28:20 +0100

    Tutorial scene done
    Yay

---
+++ GlobalGameJam2018/Assets/2. Scripts/Tutorial/PromptScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PromptScript : MonoBehaviour {
+    void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        gameObject.GetComponentInChildren<MeshRenderer>().enabled = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Player")
+        {
+            gameObject.GetComponent<SpriteRenderer>().enabled = true;
+            gameObject.GetComponentInChildren<MeshRenderer>().enabled = true;
+        }
+    }
+}
